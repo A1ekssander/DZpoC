417ffcfc9b226c88c7b9e10a8155fe74e9913352
Alexander_Korolev <ak402839@gmail.ru>
2023-07-16 23:48:26 +0300

    Решение первой задачи с урока написал

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// является ли первое число(a) квадратом второго(b)?
+
+        Console.WriteLine("Введите первое число");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if (a == b * b)
+        {
+            Console.Write("Да");
+        }
+        else
+        {
+            Console.Write("Нет");
+        }
+        
