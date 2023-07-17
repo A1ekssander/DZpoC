c76347cc52b2f30b1e9d3d2327d5f482596e64fe
Alexander_Korolev <ak402839@gmail.ru>
2023-07-17 23:42:00 +0300

    Решил задачу 2

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,3 +3,25 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+int max = 0;
+int min = 0;
+
+Console.Write("Введите первое целое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    max = firstNumber;
+    min = secondNumber;
+}
+else
+{
+    max = secondNumber;
+    min = firstNumber;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
+
+
