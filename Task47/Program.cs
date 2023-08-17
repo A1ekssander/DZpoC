a5aff35cc5e5a87b1330676b7856cebcab062a28
Alexander_Korolev <ak402839@gmail.ru>
2023-08-17 21:43:33 +0300

    попробовал решить задачу 47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -7,7 +7,6 @@
 
 double[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-   // 0 1
 double[,] matrix = new double[rows, columns]; // rows = 3, columns = 4
 
 Random rnd = new Random();
@@ -37,3 +36,25 @@ Console.WriteLine(" |");
 
 double[,] array2d = CreateMatrixRndInt(3, 4, -10, 10);
 PrintMatrix(array2d);
+
+
+
+// Console.WriteLine("Задайте количество строк двумерного массива:");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Задайте количество столбцов двумерного массива:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// double[,] twoDimArray = new double[m, n];
+// Random rnd = new Random();
+// void PrintArray(double[,] matr)
+// { for (int i = 0; i < m; i++)
+//  { for (int j = 0; j < n; j++)
+//  { Console.Write($"{matr[i, j]} ");}
+//  Console.WriteLine();}}
+
+// void FillArray(double[,] matr)
+// { for (int i = 0; i < m; i++)
+//  { for (int j = 0; j < n; j++)
+//  { matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);}}}
+// FillArray(twoDimArray);
+// Console.WriteLine();
+// PrintArray(twoDimArray);
