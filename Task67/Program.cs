158d9661e2c7ad3d48e0fd19d18d3803bfb82c70
Alexander_Korolev <ak402839@gmail.ru>
2023-08-19 19:02:55 +0300

    добавил задачу 67 с семинара

---
+++ Task67/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+//Это нахождение факториала числа через рекурсию:
+
+// int FactorialRec(int n)
+// {
+// if (n == 1) return 1;
+// else return n * FactorialRec(n - 1);
+// }
+
+// Console.WriteLine(FactorialRec(10)); // 3628800
+
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumDigit(num);
+Console.WriteLine(sum);
+
+int SumDigit(int number)
+{
+    if (number == 0) return 0;
+    return number % 10 + SumDigit(number / 10); // 453 -> 45 -> 4 | 4%10 + 45%10 + 453%10 + 0 = 12
+}
