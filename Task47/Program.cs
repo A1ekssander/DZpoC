e4356256943071d3bd5ef3c240f9d03d472cb5f9
Alexander_Korolev <ak402839@gmail.ru>
2023-08-19 11:10:25 +0300

    добавил решение с гитхаба

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -39,22 +39,39 @@ PrintMatrix(array2d);
 
 
 
-// Console.WriteLine("Задайте количество строк двумерного массива:");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Задайте количество столбцов двумерного массива:");
-// int n = Convert.ToInt32(Console.ReadLine());
-// double[,] twoDimArray = new double[m, n];
-// Random rnd = new Random();
-// void PrintArray(double[,] matr)
-// { for (int i = 0; i < m; i++)
-//  { for (int j = 0; j < n; j++)
-//  { Console.Write($"{matr[i, j]} ");}
-//  Console.WriteLine();}}
 
-// void FillArray(double[,] matr)
-// { for (int i = 0; i < m; i++)
-//  { for (int j = 0; j < n; j++)
-//  { matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);}}}
-// FillArray(twoDimArray);
-// Console.WriteLine();
-// PrintArray(twoDimArray);
+
+// Решение с гитхаба(понятное)
+
+// Console.WriteLine("введите количество строк");
+// int linesVol = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите количество столбцов");
+// int columnsVol = Convert.ToInt32(Console.ReadLine());
+// double[,] numbers = new double[linesVol, columnsVol];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+// void FillArrayRandomNumbers(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+//         }
+//     }
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine("");
+//     }
+// }
